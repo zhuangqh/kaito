9edff8d4babeca0cdc159b7d5ee41e67e78902eb
Fei Guo <guofei@microsoft.com>
2025-04-21 17:21:17 -0700

    docs: Update README for new release (#1033)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,13 +6,13 @@ variable "location" {
 
 variable "kaito_gpu_provisioner_version" {
   type        = string
-  default     = "0.3.1"
+  default     = "0.3.3"
   description = "kaito gpu provisioner version"
 }
 
 variable "kaito_workspace_version" {
   type        = string
-  default     = "0.4.4"
+  default     = "0.4.5"
   description = "kaito workspace version"
 }
 
