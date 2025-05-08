f6a1f152266589281446150e6fce5f16f7bae07a
qfai <404630507@qq.com>
2025-05-08 19:41:56 +0800

    fix: update arc GPUMem to 32 and add more examples for arc (#1111)
    **Reason for Change**:
    <!-- What does this PR improve or fix in Kaito? Why is it needed? -->
    The GPU memory size of SKU NC16 should be 32 not 48
    **Requirements**
    
    - [ ] added unit tests and e2e tests (if applicable).
    
    **Issue Fixed**:
    <!-- If this PR fixes GitHub issue 4321, add "Fixes #4321" to the next
    line. -->
    
    **Notes for Reviewers**:
    
    ---------
    
    Co-authored-by: qfai <quke@microsoft.com>

--- pkg/sku/arc_sku_handker.go
+++ pkg/sku/arc_sku_handker.go
@@ -10,12 +10,12 @@ func NewArcSKUHandler() CloudSKUHandler {
 		{SKU: "Standard_NK12", GPUCount: 2, GPUMemGB: 16, GPUModel: "NVIDIA T4"},
 		{SKU: "Standard_NC4_A2", GPUCount: 1, GPUMemGB: 16, GPUModel: "NVIDIA A2"},
 		{SKU: "Standard_NC8_A2", GPUCount: 1, GPUMemGB: 16, GPUModel: "NVIDIA A2"},
-		{SKU: "Standard_NC16_A2", GPUCount: 2, GPUMemGB: 48, GPUModel: "NVIDIA A2"},
-		{SKU: "Standard_NC32_A2", GPUCount: 2, GPUMemGB: 48, GPUModel: "NVIDIA A2"},
+		{SKU: "Standard_NC16_A2", GPUCount: 2, GPUMemGB: 32, GPUModel: "NVIDIA A2"},
+		{SKU: "Standard_NC32_A2", GPUCount: 2, GPUMemGB: 32, GPUModel: "NVIDIA A2"},
 		{SKU: "Standard_NC4_A16", GPUCount: 1, GPUMemGB: 16, GPUModel: "NVIDIA A16"},
 		{SKU: "Standard_NC8_A16", GPUCount: 1, GPUMemGB: 16, GPUModel: "NVIDIA A16"},
-		{SKU: "Standard_NC16_A16", GPUCount: 2, GPUMemGB: 48, GPUModel: "NVIDIA A16"},
-		{SKU: "Standard_NC32_A16", GPUCount: 2, GPUMemGB: 48, GPUModel: "NVIDIA A16"},
+		{SKU: "Standard_NC16_A16", GPUCount: 2, GPUMemGB: 32, GPUModel: "NVIDIA A16"},
+		{SKU: "Standard_NC32_A16", GPUCount: 2, GPUMemGB: 32, GPUModel: "NVIDIA A16"},
 	}
 	return NewGeneralSKUHandler(supportedSKUs)
 }
