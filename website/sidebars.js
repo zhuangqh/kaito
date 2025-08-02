fa41991bd6bc7a2558432a1e17f2071d6b9a04ae
Sertaç Özercan <852750+sozercan@users.noreply.github.com>
2025-08-01 17:26:03 -0700

    docs: add aikit to integrations (#1303)
    **Reason for Change**:
    <!-- What does this PR improve or fix in KAITO? Why is it needed? -->
    
    Adds AIKit as part of integrations to extend KAITO with CPU-based
    inference, and increased custom model support with a variety of
    different formats.
    
    **Requirements**
    
    - [ ] added unit tests and e2e tests (if applicable).
    
    **Issue Fixed**:
    Fixes #1212
    
    **Notes for Reviewers**:
    I'll update versioned docs after LGTM
    
    ---------
    
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -52,6 +52,14 @@ const sidebars = {
         },
         {
             type: 'category',
+            label: 'Integrations',
+            collapsed: false,
+            items: [
+                'aikit',
+            ],
+        },
+        {
+            type: 'category',
             label: 'Operations',
             collapsed: false,
             items: [
