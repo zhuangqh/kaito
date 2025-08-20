8c8585a138c4a9273de6149e184ff5b951cd4c18
Faeka Ansari <faeka6@gmail.com>
2025-08-20 23:11:18 +0530

    docs: add hideable sidebar option to Docusaurus config (#1408)
    **Reason for Change**:
    <!-- What does this PR improve or fix in KAITO? Why is it needed? -->
    
    this PR adds a hideable option to the Docusaurus sidebar configuration
    allowing users to easily toggle sidebar visibility. It improves website
    navigation and user experience by providing more flexibility
    
    **Issue Fixed**:
    <!-- If this PR fixes GitHub issue 4321, add "Fixes #4321" to the next
    line. -->
    
    No related GitHub issue; this is a minor UX enhancement with no breaking
    changes to existing website structure
    
    Signed-off-by: Faeka Ansari <faeka6@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -97,6 +97,11 @@ const config = {
                     },
                 ],
             },
+            docs: {
+                sidebar: {
+                    hideable: true,
+                },
+            },
             footer: {
                 style: 'dark',
                 links: [
