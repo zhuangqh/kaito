c96327283000368950645bfefca0983d8cba37b8
rambohe <rambohe.ch@gmail.com>
2025-10-20 03:46:30 +1100

    feat: update gpu-provisioner version to v0.3.7 for kaito (#1604)
    **Reason for Change**:
    <!-- What does this PR improve or fix in KAITO? Why is it needed? -->
    
    **Requirements**
    
    - [ ] added unit tests and e2e tests (if applicable).
    
    **Issue Fixed**:
    <!-- If this PR fixes GitHub issue 4321, add "Fixes #4321" to the next
    line. -->
    
    **Notes for Reviewers**:
    
    Signed-off-by: rambohe-ch <rambohe.ch@gmail.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,7 +18,7 @@ variable "node_pool_vm_size" {
 
 variable "kaito_gpu_provisioner_version" {
   type        = string
-  default     = "0.3.6"
+  default     = "0.3.7"
   description = "kaito gpu provisioner version"
 }
 
